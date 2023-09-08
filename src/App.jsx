0da218b92aa01e9b12f284367796fc3f094f33bd
Guru Sanjay R K <gurucoolmsd@gmail.com>
2023-09-08 10:15:21 +0530

    Added Skeleton

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+    </div>
+  )
+}
+
+export default App
